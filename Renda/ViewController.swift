c7e3fef4fee10ac8268ae10103cf3329706d4e62
Ryu Takahashi <85612232+230474-takahashi@users.noreply.github.com>
2021-08-17 10:02:29 +0900

    Initial Commit

---
+++ Renda/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Renda
+//
+//  Created by 高橋　龍 on 2021/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    @IBOutlet var topButton: UIButton!
+    
+    
+    
+    var tapCount = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        topButton.layer.cornerRadius = 125
+    }
+    
+    @IBAction func tapTapButton() {
+        tapCount = tapCount + 1
+        
+        countLabel.text = String(tapCount)
+    }
+
+
+}
+
